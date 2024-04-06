7e5569cf45bc0c1756e7c4e1a0f7c4caaea78f35
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-04-06 18:40:25 +0300

    fixed hw solution

--- src/main/java/mate/academy/rickandmorty/mapper/CharacterMapper.java
+++ src/main/java/mate/academy/rickandmorty/mapper/CharacterMapper.java
@@ -1,11 +1,8 @@
 package mate.academy.rickandmorty.mapper;
 
-import mate.academy.rickandmorty.config.MapperConfig;
 import mate.academy.rickandmorty.dto.internal.CharacterResponseDto;
 import mate.academy.rickandmorty.entity.Character;
-import org.mapstruct.Mapper;
 
-@Mapper(config = MapperConfig.class)
 public interface CharacterMapper {
     CharacterResponseDto toDto(Character character);
 }
